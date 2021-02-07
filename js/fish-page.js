adece107fb966a081aa806aa927ea40836e3c6ae
emansoliman <emyrade29@gmail.com>
2021-02-07 03:18:55 +0200

    character page v2

--- js/fish-page.js
+++ js/fish-page.js
@@ -1,8 +1,14 @@
 //canvas setup
 const canvas=document.getElementById("canvas1")
 const ctx=canvas.getContext('2d')
-canvas.width=500;
-canvas.heigth=200;
+var g=document.getElementById("canvas1")
+var canvasRect = canvas.getBoundingClientRect(); 
+
+
+canvas.width=window.innerWidth
+canvas.heigth=window.innerHeight
+console.log(window.innerWidth);
+console.log(window.innerHeight);
 
 let score =0;
 let gameFrame=0;
@@ -30,19 +36,21 @@ mouse.click=false;
 })
 //player
 const playerLeft=new Image();
-playerLeft.src="img/imageonline-co-flipped.png";
-//playerLeft.setAttribute('bordee-radius','25px')
-const playeRight=new Image();
-playeRight.src="img/imageonline-co-flipped (5).png"
+playerLeft.src="img/imageonline-co-resizedimage (3).png";
+
+// //playerLeft.setAttribute('bordee-radius','25px')
+ const playeRight=new Image();
+  playeRight.src="img/imageonline-co-flipped (8).png"
 const playedown_left=new Image();
-playedown_left.src="img/imageonline-co-flipped (4).png"
+playedown_left.src="img/imageonline-co-flipped (7).png"
 const playedown_Rigth=new Image();
-playedown_Rigth.src="img/imageonline-co-flipped (6).png"
+ playedown_Rigth.src="img/imageonline-co-flipped (9).png"
+
 class Player{
     constructor(){
         this.x=canvas.width/2;
         this.y=canvas.heigth/2;
-        this.radius=20;
+        this.radius=10;
         this.angle=0;
         this.frameX=0;
         this.frameY=0;
@@ -54,9 +62,9 @@ class Player{
 
         const dx=this.x - mouse.x;
         const dy=this.y - mouse.y;
-        let theta=Math.atan2(dy,dx);
+        //let theta=Math.atan2(dy,dx);
         //console.log(theta);
-        this.angle=theta;
+       // this.angle=theta;
         if(mouse.x!=this.x)
         {
             this.x -= dx/30;
@@ -79,45 +87,59 @@ class Player{
             ctx.stroke();
 
         }
-        ctx.fillStyle='red';
-        ctx.beginPath();
-        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
-        ctx.fill();
-        ctx.closePath()
-        ctx.fillRect(this.x,this.y,this.radius,2);
+       
+        //ctx.fillStyle='red';
+        //ctx.beginPath();
+        //ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
+       // ctx.fill();
+        //ctx.closePath()
+       // ctx.fillRect(this.x,this.y,this.radius,2);
        ctx.save();
      //   ctx.rotate(this.angle)
         ctx.translate(this.x,this.y);
         if(this.x>=mouse.x){
             
-             ctx.drawImage(playerLeft,this.frameX*this.spritewidth+2,this.frameY*this.spriteHeight+2,
-            this.spritewidth,this.spriteHeight,0-22,0-21,this.spritewidth/4,this.spriteHeight/4);
-        }
+            
+            ctx.drawImage( playerLeft,this.frameX*this.spritewidth,this.frameY*this.spriteHeight,
+                this.spritewidth,this.spriteHeight,0-52,0-61,this.spritewidth,this.spriteHeight);
+              
+          
+       }
         else if (this.x<mouse.x){
            
             
             ctx.drawImage( playeRight,this.frameX*this.spritewidth,this.frameY*this.spriteHeight,
-            this.spritewidth,this.spriteHeight,0-22,0-21,this.spritewidth/4,this.spriteHeight/4);
+            this.spritewidth,this.spriteHeight,0-60,0-65,this.spritewidth,this.spriteHeight);
 
 
         }
-         if(this.y<mouse.y && this.x>=mouse.x) 
-        {
+//          if(this.y<mouse.y && this.x>=mouse.x) 
+//         { 
+//             //    context.clearRect(0, 0, canvas.width, canvas.height);
 
-            console.log("enter");
-            ctx.drawImage(playedown_left,this.frameX*this.spritewidth,this.frameY*this.spriteHeight,
-                this.spritewidth,this.spriteHeight,0-22,0-21,this.spritewidth/4,this.spriteHeight/4);
-
-        }
-        else if(this.y<mouse.y && this.x<mouse.x) 
+//             playeRight.width=30
+//             playeRight.height=20
             
-{
-                ctx.drawImage(playedown_Rigth,this.frameX*this.spritewidth,this.frameY*this.spriteHeight,
-                    this.spritewidth,this.spriteHeight,0-22,0-21,this.spritewidth/4,this.spriteHeight/4);
+
+//             console.log("enter");
+//             ctx.drawImage(playedown_left,this.frameX*this.spritewidth,this.frameY*this.spriteHeight,
+//                 this.spritewidth,this.spriteHeight,0-52,0-61,this.spritewidth,this.spriteHeight);
+
+//         }
+//         else if(this.y<mouse.y && this.x<mouse.x) 
+              
+// {      
+//          ctx.drawImage(playedown_Rigth,this.frameX*this.spritewidth,this.frameY*this.spriteHeight,
+//                     this.spritewidth,this.spriteHeight,0-60,0-65,this.spritewidth,this.spriteHeight);
     
             
-        }
+//         }
+
+
         ctx.restore()
+       
+     
+
     }
 }
 const player=new Player();
@@ -132,6 +154,7 @@ function animate(){
    gameFrame++;
 
     requestAnimationFrame(animate);
+    //requestAnimationFrame(player.draw);
 
 }
 
