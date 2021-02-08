bffa47fcd0faa65289df62dbc0ce97d02ffe7f7e
Maymaher <mai.maher003@gmail.com>
2021-02-08 02:38:25 +0200

    player and bubbles integration v1

---
+++ js/bubble.js
@@ -0,0 +1,92 @@
+
+const bubbleArray = [];
+const bubbleImage=new Image();
+bubbleImage.src="./img/bubble_pop_frame_01.png"
+
+const player=new Player();
+
+class bubble {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = canvas.height + 100;
+        this.radius = 30;
+        this.speed = Math.random() * 7 + 1;
+        this.distance;
+        this.counted = false;
+    }
+    update() {
+        this.y -= this.speed;
+        const dx=this.x-player.x;
+        const dy=this.y-player.y;
+        this.distance = Math.sqrt(dx * dx + dy * dy)
+    }
+
+    draw() {
+        // ctx.fillStyle = "blue";
+        // ctx.beginPath();
+        // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        // ctx.fill();
+        // ctx.closePath();
+        // ctx.stroke();
+        ctx.drawImage(bubbleImage,this.x-39,this.y-39,this.radius*2.6,this.radius*2.6);
+    }
+
+}
+
+function handleBubbles() {
+    if (gameFrame % 50 == 0) {
+        bubbleArray.push(new bubble());
+
+    }
+    for (let i = 0; i < bubbleArray.length; i++) {
+        bubbleArray[i].update();
+        bubbleArray[i].draw();
+        // if(bubbleArray[i].y<0)
+        // {
+        //     bubbleArray.splice(i,1);
+        // }
+        if (bubbleArray[i].y < 0 - bubbleArray[i].radius * 2) {
+            bubbleArray.splice(i, 1);
+            i--;
+        }
+       
+            else if(bubbleArray[i].distance<bubbleArray[i].radius+player.radius)
+            {
+                if(!bubbleArray[i].counted)
+                {
+                    score++;
+                    bubbleArray[i].counted=true;
+                    bubbleArray.splice(i,1);
+                           i--;
+                }
+            }
+     
+    }
+
+   
+}
+const background=new Image();
+background.src="./img/Download Nature Scene With Coral Reef Underwater for free.jpg";
+
+function handleBackground()
+{
+   ctx.drawImage(background,0,0,canvas.width,canvas.height);
+}
+
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    handleBackground();
+    handleBubbles();
+    player.update();
+     player.draw();
+   
+    gameFrame++;
+    ctx.fillText('score:' + score, 50, 100)
+    ctx.fillStyle = "black";
+    ctx.font = "50px Arial";
+    requestAnimationFrame(animate);
+}
+
+animate();
--- js/game.js
+++ js/game.js
@@ -2,11 +2,14 @@ const canvas = document.getElementById("canvas1")
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+console.log("w="+window.innerWidth);
+console.log("h+"+window.innerHeight);
 
 let score = 0;
 let gameFrame = 0;
 
 let canvasPosition = canvas.getBoundingClientRect();
+
 
 const mouse = {
     x: canvas.width / 2,
@@ -14,94 +17,33 @@ const mouse = {
     click: false
 }
 
-canvas.addEventListener("mousedown", function (e) {
-    mouse.x = e.x - canvasPosition.left;
-    mouse.y = e.y - canvasPosition.top;
+// canvas.addEventListener("mousedown", function (e) {
+//     mouse.x = e.x - canvasPosition.left;
+//     mouse.y = e.y - canvasPosition.top;
+// })
+// console.log("cnv position==="+canvasPosition);
+canvas.addEventListener('mousemove',function(event){
+    mouse.click=true;
+    mouse.x= event.x - canvasPosition.left;
+    mouse.y= event.y - canvasPosition.top;
+    // console.log("line 22",mouse.x,mouse.y);
+    });
+    
+
+canvas.addEventListener('mouseup',function()
+{
+
+mouse.click=false;
+
 })
 
+const playerLeft=new Image();
+playerLeft.src="./img/imageonline-co-resizedimage (3).png";
 
-const bubbleArray = [];
-const bubbleImage=new Image();
-bubbleImage.src="./img/bubble_pop_frame_01.png"
-
-class bubble {
-    constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = canvas.height + 100;
-        this.radius = 30;
-        this.speed = Math.random() * 7 + 1;
-        this.distance;
-        this.counted = false;
-    }
-    update() {
-        this.y -= this.speed;
-        // const dx=this.x-player.x;
-        // const dy=this.y-player.y;
-        // this.distance = Math.sqrt(dx * dx + dy * dy)
-    }
-
-    draw() {
-        // ctx.fillStyle = "blue";
-        // ctx.beginPath();
-        // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        // ctx.fill();
-        // ctx.closePath();
-        // ctx.stroke();
-        ctx.drawImage(bubbleImage,this.x-39,this.y-39,this.radius*2.6,this.radius*2.6);
-    }
-
-}
-
-function handleBubbles() {
-    if (gameFrame % 50 == 0) {
-        bubbleArray.push(new bubble());
-
-    }
-    for (let i = 0; i < bubbleArray.length; i++) {
-        bubbleArray[i].update();
-        bubbleArray[i].draw();
-        // if(bubbleArray[i].y<0)
-        // {
-        //     bubbleArray.splice(i,1);
-        // }
-        if (bubbleArray[i].y < 0 - bubbleArray[i].radius * 2) {
-            bubbleArray.splice(i, 1);
-            i--;
-        }
-       
-            // else if(bubbleArray[i].distance<bubbleArray[i].radius+player.radius)
-            // {
-            //     if(!bubbleArray[i].counted)
-            //     {
-            //         score++;
-            //         bubbleArray[i].counted=true;
-            //         bubbleArray.splice(i,1);
-                        //    i--;
-            //     }
-            // }
-     
-    }
-
-   
-}
-const background=new Image();
-background.src="./img/Download Nature Scene With Coral Reef Underwater for free.jpg";
-
-function handleBackground()
-{
-   ctx.drawImage(background,0,0,canvas.width,canvas.height);
-}
-
-function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    handleBackground();
-    handleBubbles();
-   
-    gameFrame++;
-    ctx.fillText('score:' + score, 50, 100)
-    ctx.fillStyle = "black";
-    ctx.font = "50px Arial";
-    requestAnimationFrame(animate);
-}
-
-animate();
+playerLeft.setAttribute('bordee-radius','25px')
+ const playeRight=new Image();
+  playeRight.src="./img/imageonline-co-flipped (8).png"
+// const playedown_left=new Image();
+// playedown_left.src="./img/imageonline-co-flipped (7).png"
+// const playedown_Rigth=new Image();
+//  playedown_Rigth.src="./img/imageonline-co-flipped (9).png"
